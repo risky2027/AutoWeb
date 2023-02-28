fb293e362464bbbf682fbcd036985a918ee5e991
risky2027 <Crashday29>
2023-03-01 01:19:41 +0300

    refactor, add tests for change title and delete post

--- tests/tests_blog/test_posts.py
+++ tests/tests_blog/test_posts.py
@@ -44,6 +44,7 @@ class TestsBlogModify:
     def setup(self, browser, url):
         self.blog_page = MainPage(browser, url + Links.blog)
         self.post_modify_page = PostModifyPage(browser, url + Links.blog)
+        self.post_page = PostPage(browser, url + Links.blog)
 
     def test_create_post(self, browser, url, faker):
         title = faker.text(10)
@@ -62,3 +63,21 @@ class TestsBlogModify:
     def test_change_title(self, browser, url, create_post_for_test):
         title, _ = create_post_for_test
         self.blog_page.open_page()
+        self.blog_page.click_created_blog(title)
+
+        self.blog_page.click_edit_button()
+        self.post_page.remove_last_symbol_title()
+        self.post_page.click_submit_button()
+
+        self.blog_page.check_edited_title(title)
+
+    def test_delete_post(self, browser, url, create_post_for_test):
+        title, _ = create_post_for_test
+        self.blog_page.open_page()
+        self.blog_page.click_created_blog(title)
+
+        self.post_page.click_delete_button()
+        self.post_page.click_submit_delete_button()
+
+        self.blog_page.check_post_deleted_successfully_message()
+        self.blog_page.check_post_not_exists(title)
